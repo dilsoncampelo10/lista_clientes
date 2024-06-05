5c0284e415bce6962dd3e12c3afd2c9e4192d5d7
dilson <dilsono316@gmail.com>
2024-06-05 16:02:14 -0300

    Salvando no banco

--- app/src/main/java/app/modelo/meusclientes/view/AdicionarCliente.java
+++ app/src/main/java/app/modelo/meusclientes/view/AdicionarCliente.java
@@ -3,6 +3,8 @@ package app.modelo.meusclientes.view;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,8 @@ import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 import app.modelo.meusclientes.R;
+import app.modelo.meusclientes.controller.ClienteController;
+import app.modelo.meusclientes.model.Cliente;
 
 
 public class AdicionarCliente extends Fragment {
@@ -24,6 +28,8 @@ public class AdicionarCliente extends Fragment {
     EditText editNumero, editBairro, editCidade, editEstado;
     CheckBox chkTermos;
     Button btnCancelar, btnSalvar;
+    Cliente cliente;
+    ClienteController clienteController;
 
     public AdicionarCliente() {
     }
@@ -70,6 +76,9 @@ public class AdicionarCliente extends Fragment {
         btnCancelar = view.findViewById(R.id.btnCancelar);
         btnSalvar = view.findViewById(R.id.btnSalvar);
 
+        cliente = new Cliente();
+        clienteController = new ClienteController(getContext());
+
     }
 
     private void getEventoBotoes() {
@@ -83,6 +92,65 @@ public class AdicionarCliente extends Fragment {
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                boolean isDadosOk = true;
+
+                if(TextUtils.isEmpty(editNome.getText())){
+                    isDadosOk = false;
+                    editNome.setError("Digite o nome");
+                    editNome.requestFocus();
+                }
+                if(TextUtils.isEmpty(editEmail.getText())){
+                    isDadosOk = false;
+                    editEmail.setError("Digite o E-mail");
+                    editEmail.requestFocus();
+                }
+
+                if(TextUtils.isEmpty(editTelefone.getText())){
+                    isDadosOk = false;
+                    editTelefone.setError("Digite o Telefone");
+                    editTelefone.requestFocus();
+                }
+
+                if(TextUtils.isEmpty(editCep.getText())){
+                    isDadosOk = false;
+                    editCep.setError("Digite o CEP");
+                    editCep.requestFocus();
+                }
+                if(TextUtils.isEmpty(editLogradouro.getText())){
+                    isDadosOk = false;
+                    editLogradouro.setError("Digite o Logradouro");
+                    editLogradouro.requestFocus();
+                }
+                if(TextUtils.isEmpty(editCidade.getText())){
+                    isDadosOk = false;
+                    editCidade.setError("Digite a Cidade");
+                    editCidade.requestFocus();
+                }
+                if(TextUtils.isEmpty(editBairro.getText())){
+                    isDadosOk = false;
+                    editBairro.setError("Digite o Bairro");
+                    editBairro.requestFocus();
+                }
+                if(TextUtils.isEmpty(editEstado.getText())){
+                    isDadosOk = false;
+                    editEstado.setError("Digite o Estado");
+                    editEstado.requestFocus();
+                }
+
+                if(isDadosOk){
+                    cliente.setNome(editNome.getText().toString());
+                    cliente.setEmail(editEmail.getText().toString());
+                    cliente.setTelefone(editTelefone.getText().toString());
+                    cliente.setCep(editCep.getText().toString());
+                    cliente.setLogradouro(editLogradouro.getText().toString());
+                    cliente.setNumero(editNumero.getText().toString());
+                    cliente.setCidade(editCidade.getText().toString());
+                    cliente.setBairro(editBairro.getText().toString());
+                    cliente.setEstado(editEstado.getText().toString());
+                    cliente.setTermosDeUso(chkTermos.isChecked());
+                    clienteController.create(cliente);
+                    Log.d("SALVANDO", "onClick: Salvo no banco de dados");
+                }
 
             }
         });
